9d13ea45a48d8073ff489c5b9a4658712621a024
John Lao <john.lao@gmail.com>
2016-11-28 22:58:34 +0800

    Add project files.

---
+++ school/app.js
@@ -0,0 +1,14 @@
+﻿(function () {
+
+    const port = 8881;
+
+    let http = require('http');
+    let express = require('express');
+    let app = express();
+
+    app.use(express.static('./public'));
+    app.listen(port, function (error) {
+        console.log(`started at port ${port}`);
+    });
+
+})();
